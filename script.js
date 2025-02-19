e19ca790afa71df9b677e48570bcace54c1e4817
BernardWang21 <bernardwang21@gmail.com>
2025-02-19 00:23:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Get the button element by its ID
+const button = document.getElementById('clickMe');
+
+// Add an event listener to the button
+button.addEventListener('click', function() {
+    alert('Button clicked!');
+});
